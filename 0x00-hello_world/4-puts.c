e117dad7c4181ba929c5445b22e53f8dc26bec3f
Rx Chris Ameh <chris1jnr@gmail.com>
2024-06-20 15:02:09 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - prints "Programming is like building a multilingual 
- * puzzle, followed by a new line."
- * return 0.
+ * main - entry point
+ *
+ * Return: Always 0 (success)
  */
-
 int main(void)
-
 {
 	puts("\"Programming is like building a multilingual puzzle");
-
-	return(0);
+	return (0);
 }    
