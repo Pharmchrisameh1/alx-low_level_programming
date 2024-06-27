4a35a6d8ffc6668a2de6c9e4f3a87aadfc139ab1
Rx Chris Ameh <chris1jnr@gmail.com>
2024-06-27 12:20:37 -0700

    detect largest

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,13 +12,18 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
+	if (a > b && a > c)
+	{
 	largest = a;
-
-	if (b > largest)
-		largest = b;
-
-	if (c > largest)
-		largest = c;
+	}
+	else if (b > c)
+	{
+	largest = b;
+	}
+	else
+	{
+	largest = c;
+	}
 
 	return (largest);
 }
