f827f1e706f04f5b0a9cbb44fc4f9f66341ce9c1
Rx Chris Ameh <chris1jnr@gmail.com>
2024-06-27 17:49:00 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print all single digits except 2 and 4
+ */
+
+void print_most_numbers(void)
+{
+	int number;
+
+	for (number = 0; number <= 9; number++)
+	{
+		if (number == 2 || number == 4)
+			continue;
+
+		_putchar((number % 10) + '0');
+	}
+
+	_putchar('\n');
+}
