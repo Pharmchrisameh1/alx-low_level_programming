80640d82fbd391b3e45b1c6ad2f70d40152cbbfa
Rx Chris Ameh <chris1jnr@gmail.com>
2024-07-01 16:19:34 -0700

    1-swap.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,18 +1,16 @@
-#ifndef MAΙΝ Η
-
+#ifndef MAIN_H
 #define MAIN_H
 
 #include <stdio.h>
-
 #include <unistd.h>
 
-int_putchar(char c);
+int _putchar(char c);
 
 void reset_to_98(int *n);
 
 void swap_int(int *a, int *b);
 
-int_strlen(char *s);
+int _strlen(char *s);
 
 void _puts(char *str);
 
