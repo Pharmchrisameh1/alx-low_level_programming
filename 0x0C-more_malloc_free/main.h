68e50047e7396ac9a1c36e1e191c37aa8bffc52b
Rx Chris Ameh <chris1jnr@gmail.com>
2024-07-18 12:17:52 -0700

    main.h

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,13 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+int _putchar(char c);
+void *malloc_checked(unsigned int b);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+int check_digit(char *s);
+int _atoi(char *s);
+void print_number(unsigned int n);
+
+#endif
