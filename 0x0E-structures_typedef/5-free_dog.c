e48535311d106776601617029671ae8122506f67
Rx Chris Ameh <chris1jnr@gmail.com>
2024-07-22 21:31:09 -0700

    5-free_dog.c

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -2,17 +2,16 @@
 #include <stdlib.h>
 
 /**
- * free_dog - frees dogs.
- * @d: struct dog.
+ * free_dog - free a dog
+ * @d: pointer to the dog
  *
- * Return: no return.
  */
 void free_dog(dog_t *d)
 {
 	if (d)
 	{
-		free(d->name);
 		free(d->owner);
+		free(d->name);
 		free(d);
 	}
 }
