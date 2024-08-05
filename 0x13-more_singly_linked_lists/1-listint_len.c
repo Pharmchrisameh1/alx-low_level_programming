a8031b110e977ef42c0c5eac08ec66e120ef9770
Rx Chris Ameh <chris1jnr@gmail.com>
2024-08-05 04:19:43 -0700

    listint_len.c

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * listint_len - check the length of the list
+ * @h: pointer to the first element of the list
+ *
+ * Return: the number of elements
+ */
+size_t listint_len(const listint_t *h)
+{
+	if (h == NULL)
+		return (0);
+	return (1 + listint_len(h->next));
